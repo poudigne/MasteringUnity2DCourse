8190aaea0445e3666366c56e762c90627d0d9313
poudigne <pierluc0414@live.ca>
2015-03-11 22:01:49 -0400

    2D RPG Course
    Added animated sprite, background, camera follow, player controler, GUI
    script to switch scene.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+  private GameObject playerSprite;
+  private Rigidbody2D playerRigidbody2D;
+  private Animator anim;
+  
+  private float movePlayerVector;
+  private bool facingRight;
+
+  public float speed = 1.4f;
+
+  void Awake()
+  {
+    playerSprite = transform.Find("PlayerSprite").gameObject;
+    playerRigidbody2D = GetComponent<Rigidbody2D>();
+    anim = playerSprite.GetComponent<Animator>();
+  }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	  movePlayerVector = Input.GetAxis("Horizontal");
+    playerRigidbody2D.velocity = new Vector2(movePlayerVector * speed, playerRigidbody2D.velocity.y);
+
+    anim.SetFloat("speed", Mathf.Abs(movePlayerVector));
+
+	  if ((movePlayerVector > 0 && !facingRight) || (movePlayerVector < 0 && facingRight))
+	    Flip();
+	}
+
+  void Flip()
+  {
+    facingRight = !facingRight;
+
+    Vector3 theScale = playerSprite.transform.localScale;
+    theScale.x *= -1;
+    playerSprite.transform.localScale = theScale;
+  }
+
+
+}
---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+  // publics
+  public float xMargin = 1.5f;
+  public float yMargin = 1.5f;
+  public float xSmooth = 1.5f;
+  public float ySmooth = 1.5f;
+
+  public Vector2 maxXandY;
+  public Vector2 minXandY;
+
+  public Transform player;
+
+  // privates
+
+
+  void Awake()
+  {
+    player = GameObject.Find("Player").transform;
+    if (player == null)
+    {
+      Debug.LogError("Player object not found");
+    } 
+  }
+
+	// Use this for initialization
+	void Start () {
+	  
+	}
+
+  void FixedUpdate()
+  {
+    float targetX = transform.position.x;
+    float targetY = transform.position.y;
+
+    if (CheckXMargin())
+    {
+      targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth*Time.fixedDeltaTime);
+    }
+
+    if (CheckYMargin())
+    {
+      targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.fixedDeltaTime);
+    }
+
+    targetX = Mathf.Clamp(targetX, minXandY.x, maxXandY.x);
+    targetY = Mathf.Clamp(targetY, minXandY.y, maxXandY.y);
+
+    transform.position = new Vector3(targetX, targetY, transform.position.z);
+  }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+  bool CheckXMargin()
+  {
+    return Mathf.Abs(transform.position.x - player.position.x) > xMargin;
+  }
+
+  bool CheckYMargin()
+  {
+    return Mathf.Abs(transform.position.y - player.position.y) > yMargin;
+  }
+}
